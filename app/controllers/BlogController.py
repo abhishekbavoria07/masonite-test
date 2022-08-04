fe898167f5faafedd9188435c3688e55c2c69ee4
offsureit <abhishek.offsureit@gmail.com>
2022-08-04 16:34:02 +0530

    changes commit

--- app/controllers/BlogController.py
+++ app/controllers/BlogController.py
@@ -1,7 +1,18 @@
 from masonite.controllers import Controller
 from masonite.views import View
-
+from masonite.request import Request
+from app.models.Post import Post
 
 class BlogController(Controller):
     def show(self, view: View):
         return view.render("blog")
+
+    # new store method
+    def store(self, request: Request):
+        Post.create(
+            title = request.input('title'),
+            body = request.input('body'),
+            author_id = request.user().id
+        )
+
+        return 'post created'
--- routes/web.py
+++ routes/web.py
@@ -6,6 +6,12 @@ ROUTES = [
     Route.get('/', 'WelcomeController@show').name('welcome'),
 
     # Blog Routes
-    Route.get('/blog', 'BlogController@show')
+    Route.get('/blog', 'BlogController@show'),
+    Route.post('/blog/create', 'BlogController@store'),
+    Route.get('/posts', 'PostController@show'),
+    Route.get('/post/@id', 'PostController@single'),
+    Route.get('/post/@id/update', 'PostController@update'),
+    Route.get('/post/@id/update','PostController@store'),
+    Route.get('/post/@id/delete', 'PostController@delete')
 ]
 ROUTES += Auth.routes()
